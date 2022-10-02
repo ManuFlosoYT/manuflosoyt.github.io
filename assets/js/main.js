f8bae28de1f849fe2f74c14fcefb18779b11652c
ManuFlosoYT <manubh94y@gmail.com>
2022-10-02 10:13:17 +0200

    custom

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,30 +231,6 @@
   });
 
   /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
    * Testimonials slider
    */
   new Swiper('.testimonials-slider', {
